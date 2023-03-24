e216536d9b0fa2e3ceb1f1e08841a4e50a477d7e
Rupika <rupikasinha23@gmail.com>
2023-03-25 00:51:35 +0530

    Made the code cleaner

--- back-end/app.py
+++ back-end/app.py
@@ -1,140 +1,77 @@
-from flask import Flask,render_template,request,redirect,url_for,session,jsonify
-import db
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from db_services import *
 
 app = Flask(__name__)
 
-
 @app.route('/get_all_books',methods={'GET'})
 def get_all_books():
-
-    # get query params
-    # how to get get parameter and post parameter from flast request object
+    """ 
+    to fetch all the books filtered by certain parameters
+    """
     user_id = request.args.get('user_id')
     donation_status = request.args.get('donation_status')
-    # print(user_id,donation_status)
-    result = db.get_all_books(user_id, donation_status)
-    # result={"user_id":user_id}
+    result = get_all_books(user_id = user_id, donation_status = donation_status)
     return jsonify(result)
 
-
-
-# add_new_book(user_id, book_name, author, genre, description, status)
 @app.route('/add_new_book',methods={'POST'})
 def add_new_book():
-#    if request.method=='POST':
-   
-    data=request.get_json()
-    user_id = data['user_id']
-    book_name=data['book_name']
-    author=data['author']
-    genre=data['genre']
-    description=data['description']
 
+    """
+    to add a new book donated by a user
+    """
+    data = request.get_json()
+    user_id = data['user_id']
+    book_name = data['book_name']
+    author = data['author']
+    genre = data['genre']
+    description = data['description']
     donation_status = data['donation_status']
-    result=db.add_new_book(user_id, book_name, author, genre, description, donation_status)
+    result = add_new_book(user_id = user_id, book_name = book_name, author = author,
+                          genre = genre, description = description, donation_status = donation_status)
     return jsonify(result)
 
 @app.route('/update_request_for_book',methods={'POST'})
 def update_request_for_book():
-#    if request.method=='POST':
-   
-    data=request.get_json()
+    """
+    add a request for a book by needy
+    """
+    data = request.get_json()
     request_user_id = data['user_id']
-    book_id=data['book_id']
-    
-
-    
-    result=db.update_request_for_book(book_id,request_user_id)
+    book_id = data['book_id']
+    result = update_request_for_book(book_id = book_id, request_user_id = request_user_id)
     return jsonify(result)
 
 
 @app.route('/get_requested_items',methods={'GET'})
 def get_requested_items():
-
-    # get query params
-    # how to get get parameter and post parameter from flast request object
+    """
+    fetch the requested items by a particular needy
+    """
     request_user_id = request.args.get('user_id')
-    
-    # print(user_id,donation_status)
-    result = db.get_requested_items(request_user_id)
-    # result={"user_id":user_id}
+    result = get_requested_items(request_user_id = request_user_id)
     return jsonify(result)
 
-app.secret_key="super secret key"
-@app.route('/register',methods={'GET','POST'})
-def index():
+@app.route('/get_needy_info',methods={'GET'})
+def get_needy_info():
+    """
+    get list of interested needy for each book donated
+    """
+    book_id = request.args.get('book_id')   
+    result = get_needy_info(book_id = book_id)
+    print(result)
+    return jsonify(result)
 
-    
-    if request.method=='POST':
-        userdetails=request.form
-        name=userdetails['name']
-        password=userdetails['password']
-        location=userdetails['location']
-        cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO user(user_name,user_pass,location) VALUES(%s,%s,%s)",(name,password,location))
-        
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('login'))
-    return render_template('index.html')
-@app.route('/login',methods={'GET','POST'})
+@app.route('/accept_book_request',methods={'POST'})
+def accept_book_request():
 
-def login():
-
-    
-    if request.method=='POST':
-        
-        userdetails=request.form
-        name=userdetails['name']
-        password=userdetails['password']
-        print(name,password)
-        cur=mysql.connection.cursor()
-        cur.execute('SELECT *FROM user WHERE user_name=%s AND user_pass=%s',(name,password))
-        record=cur.fetchone()
-        print(record)
-        if record:
-            print('1')
-            session['loggedin']=True
-            session['user_id']=record[0]
-            return redirect(url_for('book'))
-        else:
-            msg="INCOREECT DETAILS"
-     
-       
-    return render_template('login.html')
-
-@app.route('/book',methods={'GET','POST'})
-def book():
-    if request.method=='POST':
-        book=request.form
-        name=book['name']
-        author=book['author']
-        genre=book['genre']
-        user_id=session['user_id']
-        cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO book(book_name,author,genre,user_id) VALUES(%s,%s,%s,%s)",(name,author,genre,user_id))
-        mysql.connection.commit()
-        cur.close()
-        return 'book_added'
-    return render_template('book.html',user_id=session['user_id'])
-# @app.route('/test',methods={'GET','POST'})
-# def test():
-#    d={
-#        '1':'book_name','2':'author'
-#    }
-#    return jsonify(d)
-
-@app.route('/needy_dis',methods={'GET','POST'})
-def needy_dis():
-    cur = mysql.connection.cursor()
-    query = "SELECT * FROM book"
-    cur.execute(query)
-    books = cur.fetchall()
-    print("All Available Books: ")
-    for book in books:
-        print(book)
-    cur.close()
+    """
+       accept a book request
+    """
+    data = request.get_json()
+    request_id = data['request_id']
+    book_id = data['book_id']
+    result = accept_book_request(book_id = book_id, request_id = request_id)
+    return jsonify(result)
 
        
 if __name__=='__main__':
--- back-end/db_services.py
+++ back-end/db_services.py
@@ -1,8 +1,17 @@
 from db_connector import *
 from classes import *
-from json_utility import *
 
 def get_all_books(user_id = None, donation_status = None) -> list:
+    """Gets all details of all books for given user id or donation status or both
+
+    Args:
+        user_id (int, optional): Numeric key used to identify a user. Defaults to None.
+        donation_status (str, optional): Pending or Accepted depening on 
+        whether book still available or donated. Defaults to '.
+
+    Returns:
+        list: a list of dictionaries containing all details of selected books
+    """
     db_connection = get_db_connection()
     cur = db_connection.cursor()
     
@@ -37,115 +46,115 @@ def get_all_books(user_id = None, donation_status = None) -> list:
     print(display_result)
     return display_result
 
-def update_request_for_book(book_id, request_user_id):
-    # prepare update query for book table
-    db_connec=get_db_connection()
-    cur = db_connec.cursor()
+def update_request_for_book(book_id = None, request_user_id = None) -> dict:
+    """Updates the request count (number of requests made) for a specific book
+
+    Args:
+        book_id (int, optional): Numeric key for identifying book. Defaults to None.
+        request_user_id (int, optional): Numeric key for identifying user who made request.
+                                            Defaults to None.
+
+    Returns:
+        dict: gives status of task completion and dictionary of details of the request added
+    """
+    db_connection = get_db_connection()
+    cur = db_connection.cursor()
     select_query = 'SELECT request_count FROM book WHERE book_id=%s'
     cur.execute(select_query,(book_id,))
-    cnt=cur.fetchall()
-
-    req_cnt  =cnt[0][0]+ 1
-    update_book_q = "UPDATE book SET request_count=%s WHERE book_id=%s"
-    cur.execute(update_book_q,(req_cnt,book_id))
-    db_connec.commit()
+    count = cur.fetchall()
+    request_count  = count[0][0] + 1
+    update_query = "UPDATE book SET request_count=%s WHERE book_id=%s"
+    cur.execute(update_query,(request_count, book_id))
+    db_connection.commit()
     # prepare add to request table
-
-    request_tbl_q = " INSERT INTO request(request_user_id,book_id,queue_order) VALUES(%s,%s,%s)"
+    insert_query = " INSERT INTO request(request_user_id,book_id,queue_order) VALUES(%s,%s,%s)"
     try:
-        
-        cur.execute(request_tbl_q,(request_user_id,book_id,req_cnt))
-        db_connec.commit()
+        cur.execute(insert_query, (request_user_id, book_id, request_count))
+        db_connection.commit()
     except:
         return {'status': False, 'error': "Failed to insert"}
     # search the insert request
-    search_query="SELECT request_id,request_user_id,book_id,queue_order FROM request WHERE  request_user_id=%s and book_id=%s and queue_order=%s"
-    cur.execute(search_query,(request_user_id,book_id,req_cnt))
-    rows=cur.fetchall()
-    
+    search_query = "SELECT request_id,request_user_id,book_id,queue_order FROM request WHERE  request_user_id=%s and book_id=%s and queue_order=%s"
+    cur.execute(search_query, (request_user_id, book_id, request_count))
+    rows = cur.fetchall()
     print(rows)
-    return{
-        'stauts': True,
-        'request_added': {
-            'request_id': rows[0][0],
-            'book_id': rows[0][2],
-            'queue_order':rows[0][3]
-#            ....
-        }
-    }
-# update_request_for_book(7, 1)
-# update_request_for_book()
-def add_new_book(user_id, book_name, author, genre, description, status):
-    db_connec=get_db_connection()
-    cur = db_connec.cursor()
+    request_object = Request(request_id = rows[0][0],
+                             request_user_id = rows[0][1],
+                             book_id = rows[0][2],
+                             queue_order = rows[0][3],
+                             request_status = 'PENDING')
+    return {'status': True, 'request_added': request_object.get_json()}
+
+def add_new_book(user_id = None, book_name = None, author = None, genre = None,
+                 description = None, status = 'PENDING') -> dict:
+    """Adds new book with given details to Book table
+
+    Args:
+        user_id (int, optional): numeric key to identify user. Defaults to None.
+        book_name (str, optional): name of the book. Defaults to None.
+        author (str, optional): name of the author. Defaults to None.
+        genre (str, optional): name of the genre the book belongs to. Defaults to None.
+        description (str, optional): brief description of the book. Defaults to None.
+        status (str, optional): status of availability/donation. Defaults to 'PENDING'.
+
+    Returns:
+        dict: returns status of insertion and dictionary of details of added book.
+    """
+    db_connection = get_db_connection()
+    cur = db_connection.cursor()
     query = "INSERT INTO book(book_name,description,author,genre,donation_status,user_id) VALUES (%s,%s,%s,%s,%s,%s)"
-    # try:
-        
-       
-    # except:
-    #         return {'status': False,'error': "Failed to insert"}
-    cur.execute(query, (book_name, description,
-                         author, genre, status, user_id))
-    db_connec.commit()   
-    query="SELECT book_id,book_name,author,genre,donation_status,user_id,request_count,description from book where book_name=%s and user_id=%s"
+    cur.execute(query, (book_name, description, author, genre, status, user_id))
+    db_connection.commit()   
+    query = "SELECT book_id,book_name,author,genre,donation_status,user_id,request_count,description from book where book_name=%s and user_id=%s"
     val = (book_name, user_id)
     cur.execute(query, val)
-    rows=cur.fetchall()
+    rows = cur.fetchall()
     print (rows)
-    return {
-        'status': True,
-        'inserted_book': {
-            'book_id': rows[0][0],
-            'book_name': rows[0][1],
-            'author':rows[0][2],
-            'genre':rows[0][3],
-            'donation_status':rows[0][4],
-            'user_id':rows[0][5],
-            'request_count':rows[0][6],
-            'description':rows[0][7]
-        }
-    }
-
-# add_new_book(1,'pj','rr','fic','def','pending')
+    book_object = Book(book_id = rows[0][0],
+            book_name = rows[0][1],
+            author = rows[0][2],
+            genre = rows[0][3],
+            donation_status = rows[0][4],
+            request_count = rows[0][5],
+            user_id = rows[0][6],
+            description = rows[0][7])
+    return {'status': True, 'inserted_book': book_object.get_json()}
+#remaining portion begins
 def get_requested_items(request_user_id):
-        db_connec=get_db_connection()
-        cur = db_connec.cursor()
-        query="Select book.book_id,book_name,author,genre,queue_order,request_status,user_name,email_id from book left join request on book.book_id=request.book_id left join user on book.user_id=user.user_id WHERE request_user_id=%s"
-        cur.execute(query,(request_user_id,))
-        rows=cur.fetchall()
+        db_connection = get_db_connection()
+        cur = db_connection.cursor()
+        query = "Select book.book_id,book_name,author,genre,queue_order,request_status,user_name,email_id from book left join request on book.book_id=request.book_id left join user on book.user_id=user.user_id WHERE request_user_id=%s"
+        cur.execute(query, (request_user_id,))
+        rows = cur.fetchall()
         result = []
         for row in rows:
-        
-       
           entry = {
             'book_id': row[0],
             'book_name': row[1],
-            
             'author':row[2],
-            
             'genre':row[3],
             'queue_order':row[4],
             'request_status':row[5],
             'user_name':row[6],
             'email_id':row[7]
-
-             
-
-            #...
             }
         result.append(entry)
         print(result)
         return result
-# get_requested_items(1)
+#remaining portion continued
 def get_needy_info(book_id):
-    db_connec=get_db_connection()
-    cur = db_connec.cursor()
+    db_connection = get_db_connection()
+    cur = db_connection.cursor()
     query="Select * from request left join user on request.request_user_id=user.user_id WHERE book_id=%s"
-    cur.execute(query,(book_id,))
-    rows=cur.fetchall()
+    cur.execute(query, (book_id, ))
+    rows = cur.fetchall()
     result = []
     for row in rows:
-        
+        request_object = Request(request_id = row[0],
+                             request_user_id = row[1],
+                             book_id = row[2],
+                             queue_order = row[3],
+                             request_status = 'PENDING')
        
         entry = {
             'request_id': row[0],
@@ -161,17 +170,24 @@ def get_needy_info(book_id):
         result.append(entry)
     print(result)
     return result
-# get_needy_info(15)
-def accept_book_request(book_id,request_id):
-    db_connec=get_db_connection()
-    cur = db_connec.cursor()
+#remaining portion ends
+def accept_book_request(book_id = None, request_id = None) -> str:
+    """Sets status of a book to have been accepted by needy
+
+    Args:
+        book_id (int, optional): numeric key to identify book. Defaults to None.
+        request_id (int, optional): numeric key to identify request. Defaults to None.
+
+    Returns:
+        str: returns message/prompt of completion
+    """
+    db_connection = get_db_connection()
+    cur = db_connection.cursor()
     update_request = "UPDATE request SET request_status=%s WHERE request_id=%s"
-    cur.execute(update_request,("COMPLETED",request_id))
-    db_connec.commit()
+    cur.execute(update_request, ("COMPLETED", request_id))
+    db_connection.commit()
     update_book = "UPDATE book SET donation_status=%s WHERE book_id=%s"
-    cur.execute(update_book,("COMPLETED",book_id))
-    db_connec.commit()
-    result="donation completed"
+    cur.execute(update_book, ("COMPLETED", book_id))
+    db_connection.commit()
+    result = "Donation Completed"
     return result
-      
-# accept_book_request(15,49)
